fd64ca1f73b49ad99cdad879bbb675a0fee8d655
dogmax <ivanov1maxim@gmail.com>
2022-02-09 14:39:55 +0200

    Implement ConsoleGameOverHandler

--- src/main/java/guess_the_number/component/console/ConsoleGameOverHandler.java
+++ src/main/java/guess_the_number/component/console/ConsoleGameOverHandler.java
@@ -1,5 +1,6 @@
 package guess_the_number.component.console;
 
+import guess_the_number.component.DataPrinter;
 import guess_the_number.component.GameOverHandler;
 
 /**
@@ -7,8 +8,14 @@ import guess_the_number.component.GameOverHandler;
  * @link https://github.com/dogmax296
  */
 public class ConsoleGameOverHandler implements GameOverHandler {
+    private final DataPrinter dataPrinter;
+
+    public ConsoleGameOverHandler(final DataPrinter dataPrinter) {
+        this.dataPrinter = dataPrinter;
+    }
+
     @Override
     public void gameOver() {
-
+        dataPrinter.printInfoMessage("GAME OVER!");
     }
 }
