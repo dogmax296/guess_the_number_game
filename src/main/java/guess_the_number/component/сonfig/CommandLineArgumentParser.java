1f384abaa07f33b550e2e152524f19326f4a84e6
dogmax <ivanov1maxim@gmail.com>
2022-02-09 14:44:52 +0200

    Implement CommandLineArgumentParser

--- src/main/java/guess_the_number/component/сonfig/CommandLineArgumentParser.java
+++ src/main/java/guess_the_number/component/сonfig/CommandLineArgumentParser.java
@@ -1,8 +1,53 @@
 package guess_the_number.component.сonfig;
 
+import guess_the_number.model.config.UserInterface;
+
+import static guess_the_number.model.config.UserInterface.CONSOLE;
+import static guess_the_number.model.config.UserInterface.GUI;
+
 /**
  * @author dogmax296
  * @link https://github.com/dogmax296
  */
 public class CommandLineArgumentParser {
+
+    final String[] args;
+
+    public CommandLineArgumentParser(final String[] args) {
+        this.args = args;
+    }
+
+    public CommandLineArguments parse() {
+        UserInterface userInterface = null;
+
+        for (final String arg : args) {
+            if (CONSOLE.name().equalsIgnoreCase(arg) || GUI.name().equalsIgnoreCase(arg)) {
+                if (userInterface == null) {
+                    userInterface = UserInterface.valueOf(arg.toUpperCase());
+                }
+            } else {
+                System.err.printf(
+                        "Unsupported command line argument: '%s'%n",
+                        arg);
+            }
+        }
+
+        if(userInterface == null){
+            userInterface = CONSOLE;
+        }
+
+        return new CommandLineArguments(userInterface);
+    }
+
+    public static class CommandLineArguments {
+        private final UserInterface userInterface;
+
+        public CommandLineArguments(final UserInterface userInterface) {
+            this.userInterface = userInterface;
+        }
+
+        public UserInterface getUserInterface() {
+            return userInterface;
+        }
+    }
 }
