36a21b82a6493e1d49016d3483d2e413693da711
dogmax <ivanov1maxim@gmail.com>
2022-02-09 14:43:29 +0200

    Implement ConsoleUserInput

--- src/main/java/guess_the_number/component/console/ConsoleUserInput.java
+++ src/main/java/guess_the_number/component/console/ConsoleUserInput.java
@@ -1,14 +1,34 @@
 package guess_the_number.component.console;
 
+import guess_the_number.component.DataPrinter;
 import guess_the_number.component.Input;
+
+import java.util.Scanner;
 
 /**
  * @author dogmax296
  * @link https://github.com/dogmax296
  */
 public class ConsoleUserInput implements Input {
+
+    private final DataPrinter dataPrinter;
+
+    public ConsoleUserInput(final DataPrinter dataPrinter) {
+        this.dataPrinter = dataPrinter;
+    }
+
     @Override
     public int guess() {
-        return 0;
+        while (true) {
+            int number = new Scanner(System.in).nextInt();
+            if (number < 0) {
+                dataPrinter.printInfoMessage("number < " + number + ". Try again:");
+            } else if (number > 9) {
+                dataPrinter.printInfoMessage("number > " + number + ". Try again:");
+            } else {
+                return number;
+            }
+        }
     }
+
 }
